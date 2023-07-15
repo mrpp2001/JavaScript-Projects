9238b18574c7b8d68ece03a9ff2b50423092d56e
Pratik Patil <pratiknpatil2001@gmail.com>
2023-07-15 15:54:46 +0530

    Added new feature script.js
    Added Audio
    Rewritten code

--- Dictionary App/script.js
+++ Dictionary App/script.js
@@ -1,50 +1,82 @@
-const form = document.querySelector('form');
-const resultDiv = document.querySelector('.result');
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    getWordInfo(form.elements[0].value);
-});
+const form = document.querySelector("form");
+const resultDiv = document.querySelector(".result");
 
-const getWordInfo = async (word) => {
-    try {
-        resultDiv.innerHTML = "Featching Data...";
-    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+form.addEventListener("submit", handleSubmit);
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  const word = form.elements[0].value;
+  try {
+    resultDiv.innerHTML = "Fetching Data...";
+    const response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    );
     const data = await response.json();
-    let definitions = data[0].meanings[0].definitions[0];
 
-
-    resultDiv.innerHTML = `
-    <h2><strong>Word: </strong>${data[0].word}</h2>
-    <p class="partOfSpeech">${data[0].meanings[0].partOfSpeech}</p>
-    <p><strong>Meaning: </strong>${definitions.definition === undefined ? "Not Found" : definitions.definition}</p>
-    <p><strong>Example: </strong>${definitions.example === undefined ? "Not Found" : definitions.example}</p>
-    <p><strong>Antonyms: </strong></p>
-    `;
-
-    // Fetching Antonyms
-    if(definitions.antonyms.length === 0) {
-        resultDiv.innerHTML += `<span>Not Found</span>`;
-    }
-else{
-    for(let i=0; i<definitions.antonyms.length; i++) {
-        resultDiv.innerHTML += `<li>${definitions.antonyms[i]}</li>`
-    }
+    displayWordInfo(data);
+  } catch (error) {
+    displayErrorMessage();
+  }
 }
 
-    //Adding Read More Button
-    resultDiv.innerHTML += `<div><a href="${data[0].sourceUrls}" target="_blank">Read More</a></div>`;
+function displayWordInfo(data) {
+  const word = data[0].word;
+  const partOfSpeech = data[0].meanings[0].partOfSpeech;
+  const definitions = data[0].meanings[0].definitions;
+  const synonyms = data[0].meanings[0].synonyms;
+  const antonyms = data[0].meanings[0].antonyms;
+  const sourceUrls = data[0].sourceUrls;
+  const phonetics = data[0].phonetics;
+  
+  let definitionsHTML = "";
+  definitionsHTML += `<h2><strong>Word: </strong>${word}</h2>`;
+  definitionsHTML += `<p class="partOfSpeech">${partOfSpeech}</p>`;
+
+  if (definitions.length > 0) {
+    definitionsHTML += `<p><strong>Meaning: </strong>${definitions[0].definition || "Not Found"}</p>`;
+    definitionsHTML += `<p><strong>Example: </strong>${definitions[0].example || "Not Found"}</p>`;
+  } else {
+    definitionsHTML += `<p><strong>Meaning: </strong>Not Found</p>`;
+  }
+
+  if (antonyms && antonyms.length > 0) {
+    definitionsHTML += `<p><strong>Antonyms: </strong><ul>${antonyms.map(antonym => `<li>${antonym}</li>`).join("")}</ul></p>`;
+  } else {
+    definitionsHTML += `<p><strong>Antonyms: </strong>Not Found</p>`;
+  }
+
+  if (synonyms && synonyms.length > 0) {
+    definitionsHTML += `<p><strong>Synonyms: </strong><ul>${synonyms.map(synonym => `<li>${synonym}</li>`).join("")}</ul></p>`;
+  } else {
+    definitionsHTML += `<p><strong>Synonyms: </strong>Not Found</p>`;
+  }
+
+  definitionsHTML += `<div><a href="${sourceUrls}" target="_blank">Read More</a></div>`;
+
+  if (phonetics && phonetics.length > 0) {
+    definitionsHTML += `<div><strong>Pronunciations: </strong>`;
+    phonetics.forEach(phonetic => {
+      definitionsHTML += `<button class="playButton" data-audio="${phonetic.audio}"><i class="fa-solid fa-volume-high"></i></button>`;
+    });
+    definitionsHTML += `</div>`;
+  }
+
+  resultDiv.innerHTML = definitionsHTML;
+
+  const playButtons = document.querySelectorAll(".playButton");
+  playButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      const audioUrl = button.getAttribute("data-audio");
+      const audio = new Audio(audioUrl);
+      audio.play();
+    });
+  });
 
 }
-catch(error){
-    resultDiv.innerHTML = `<p>Sorry, the word could not be found</p>`
+
+function displayErrorMessage() {
+  resultDiv.innerHTML = `<p>Sorry, the word could not be found</p>`;
 }
 
-    //Task To Do - Fetch Synonyms as we have fone from Antonyms
-
-    console.log(data);
-
-
-
-}
 
